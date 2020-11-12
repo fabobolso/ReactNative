dd907c96682f88f1ef5eb26964800017864741a0
fabio@parapentex.com <fabio@parapentex.com>
2020-11-12 18:35:39 -0300

    img maps

--- app/components/Restaurantes/AddRestaurantesForm.js
+++ app/components/Restaurantes/AddRestaurantesForm.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import {StyleSheet, View, Text, ScrollView, Dimensions} from "react-native";
-import {Icon, Image, Input, Button} from 'react-native-elements';
+import {Icon, Image, Input, Button, Avatar} from 'react-native-elements';
 import { Alert } from "react-native";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import {filter, map, size} from "lodash";
+
+import Modal from "../Modal";
+
+const widthScreen = Dimensions.get("window").width;
 
 
-export default function AddrestauranteForm(){
+export default function AddrestauranteForm(props){
+
+    const {toastRef, setIsLoading, navigation }= props;
+    const [restaurantadres, setRestaurantadres] = useState("");
+    const [restaurantdescrip, setRestaurantdescrip] = useState(null);
+    const [restaurantname, setRestaurantname] = useState("");
+    const [imgselected, setImgselected] = useState([]);
+    const [isVisibleMap, setIsVisibleMap] = useState(false)
+
 
     const addrestaurante = () => {
 
@@ -14,31 +29,181 @@ export default function AddrestauranteForm(){
         <ScrollView
         style={style.scroll}
         >
+            <ImgRestaurante imagerestaurant={imgselected[0]} />
 
-         <Formadd />
+         <Formadd
+         setRestaurantname={setRestaurantname}
+         setRestaurantadres={setRestaurantadres}
+         setRestaurantdescrip={setRestaurantdescrip} 
+         setIsVisibleMap={setIsVisibleMap}
+         />
+
+        <Imgadd 
+        toastRef={toastRef} 
+        imgselected={imgselected} 
+        setImgselected={setImgselected}
+        />
+
          <Button 
          title="Crear Restaurante"
          onPress={addrestaurante}
          buttonStyle={style.btnstyle}
          />
+         <Map 
+         isVisibleMap={isVisibleMap}
+         setIsVisibleMap={setIsVisibleMap}
+         />
         </ScrollView>
+    );
+}
+
+function ImgRestaurante(props){
+
+    const {imagerestaurant} = props;
+
+    return(
+        <View style={style.viewfoto}>
+            <Image source={imagerestaurant ? {uri:imagerestaurant} : require("../../../assets/img/no-image.png")}
+            style={{width: widthScreen, height:200}}
+            />
+        </View>
+    )
+
+}
+
+function Formadd(props){
+
+    const {setRestaurantname, setRestaurantadres, setRestaurantdescrip, setIsVisibleMap} = props;
+
+    return(
+        <View style={style.view}>
+
+            <Input 
+            placeholder="Nombre del restaurante" 
+            style={style.inputf}
+            onChange={(e) => setRestaurantname(e.nativeEvent.text)}
+            >                
+            </Input>
+
+            <Input placeholder="Direccion del restaurante"
+                style={style.inputf}
+            onChange={(e) => setRestaurantadres(e.nativeEvent.text)}
+            rightIcon={{
+                type:"material-community",
+                name:"google-maps",
+                color:"#c2c2c2",
+                onPress: () => setIsVisibleMap(true),
+            }}
+            >                
+            </Input>
+
+            <Input 
+            placeholder="Descripcion del restaurante" 
+            multiline={true} style={style.inputf} 
+            inputContainerStyle={style.inputconst}
+            onChange={(e) => setRestaurantdescrip(e.nativeEvent.text)}
+            >             
+            </Input>
+       
+        </View>
     )
 }
 
-function Formadd(){
+// Funcion para cargar mapa
+
+function Map(props){
+    const {isVisibleMap, setIsVisibleMap} = props;
+
     return(
-        <View style={style.view}>
-            <Input placeholder="Nombre del restaurante" style={style.inputf}></Input>
-            <Input placeholder="Direccion del restaurante" style={style.inputf}></Input>
-            <Input placeholder="Descripcion del restaurante" multiline={true} style={style.inputf} inputContainerStyle={style.inputconst} ></Input>
+        <Modal isVisible={isVisibleMap} setVisible={setIsVisibleMap}>
+            <Text>Mapa</Text>
+        </Modal>
+    )
+
+}
+
+function Imgadd(props){
+
+    const {toastRef,imgselected, setImgselected} = props;
+
+    const imageselect = async () => {
+
+       const reultpermission = await Permissions.askAsync(
+           Permissions.CAMERA_ROLL
+       );
        
+       if(reultpermission === "denied"){
+        toastRef.current.show("Es necesario aceptar los permisos de la galeria, ve a configuracion app y acepta para poder usar la galeria",1000);
+
+       }else {
+           const result = await ImagePicker.launchImageLibraryAsync({
+               allowsEditing: true,
+               aspect:[4,3]
+           });
+
+           if(result.cancelled){
+               toastRef.current.show("Has cerrado la galeria sin elegir ninguna imagen",1000)
+           }else{
+               setImgselected([...imgselected, result.uri]);
+           }
+       }
+    };
+
+    const remobeimg = (image) => {
+        
+        Alert.alert(
+            "Eliminar",
+            "¿Desea eliminar la imagen?",
+            [
+                {
+                    text: "Cancel",
+                    style:"cancel"
+                },
+                {
+                    text: "Eliminar",
+                    onPress: () => {
+                       setImgselected(
+                         filter(imgselected, (imageurl) => imageurl !== image)
+                       );
+                    },   
+                }    
+            ],
+            {cancelable: false}
+            )
+    }
+
+    return(
+                        //codigo para scroll horizontal de fotos     
+                        /*<ScrollView horizontal>
+                              { Code... }
+                            </ScrollView>*/
+
+        <View style={style.viewimg}>
+            {size(imgselected) < 4 &&(
+                <Icon
+                type="material-community"
+                name="camera"
+                color="#7a7a7a"
+                containerStyle={style.conticon}
+                onPress={imageselect}
+                />
+            )}
+            {map(imgselected, (imagerestaurant, index) => (
+            <Avatar
+            key={index}
+            style={style.minstl}
+            source={{uri: imagerestaurant }}
+            onPress={() => remobeimg(imagerestaurant)}
+            />
+
+            ))}
         </View>
     )
 }
 
 const style = StyleSheet.create({
     scroll:{
-        height:"100%"
+        height:"100%",
     },
     view:{
         marginLeft:10,
@@ -58,5 +223,30 @@ const style = StyleSheet.create({
         backgroundColor:"#00a680",
         marginTop:20,
         marginRight:5
+    },
+    viewimg:{
+        flexDirection:"row",
+        marginLeft:20,
+        marginRight:20,
+        marginTop:30
+    },
+    conticon:{
+        alignItems:"center",
+        justifyContent:"center",
+        marginRight:10,
+        width:70,
+        height:70,
+        backgroundColor:"#e3e3e3"
+        
+    },
+    minstl:{
+        width:70,
+        height:70,
+        marginRight:10
+    },
+    viewfoto:{
+        alignItems:"center",
+        height:200,
+        marginBottom:20
     }
 })
